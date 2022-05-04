e193b45a9325070ec318fbf89a2d8d9177cd1227
katok1 <Katya.komogortseva.02@mail.ru>
2022-05-04 16:21:33 +0700

    Сделаны страницы "вход" и "регистрация"

--- src/components/Auth/LogIn/LogIn.jsx
+++ src/components/Auth/LogIn/LogIn.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
-
+import style from './style.css'
 const LogIn = () => {
-	return <div>Тут рисуй страницу входа</div>
+
+	return (
+		<div className={"LogIn-container"}>
+			<div className={"LogIn-topContainer"}>Войти
+				<div className={"LogIn-content_top"}>
+					<input type="email" placeholder={"почта"}/>
+					<input type="password" placeholder={"пароль"}/>
+					<button className={"LogIn_Btn"} style={{backgroundColor: "#494949"}}> Вход</button>
+					<div className={"LogIn-text"}>  Нет аккаунта? Зарегистрироваться </div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default LogIn
--- src/components/Auth/SignUp/SignUp.jsx
+++ src/components/Auth/SignUp/SignUp.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
-
+import style from './style.css'
 const SignUp = () => {
-	return <div>Тут рисуй регистрацию</div>
+
+	return (
+		<div className={"SignUp-container"}>
+			<div className={"SignUp-topContainer"}>Регистрация
+				<div className={"SignUp-content_top"}>
+					<input type="email" placeholder={"почта"}/>
+					<input type="password" placeholder={"пароль"}/>
+					<input type="password" placeholder={"пароль"}/>
+					<button className={"SignUp_Btn"} style={{backgroundColor: "#494949"}}>Регистрация</button>
+					<div className={"SignUp-text"}>Войти</div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default SignUp
