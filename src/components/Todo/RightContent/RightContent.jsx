dedfc04b2b5433b83ccaa9b14940b9263926d3d7
Haleckiy <haleckiydenisirl@gmail.com>
2022-05-09 02:29:34 +0700

    Создано отображение всей доп информации о задаче при клике

--- src/components/Todo/RightContent/RightContent.jsx
+++ src/components/Todo/RightContent/RightContent.jsx
@@ -9,24 +9,28 @@ import ModalWindow from "./ModalWindow/ModalWindow";
 
 const RightContent = () => {
 
+	const Lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam rhoncus rhoncus elit, a rhoncus mi commodo sit amet. Sed tellus nunc, vulputate sit amet viverra ultrices, venenatis vitae tortor. Mauris cursus augue quis nisi tempor eleifend. Mauris mi velit, facilisis ut pharetra eu, dignissim sed nisi. Praesent dapibus pharetra rutrum. Quisque accumsan malesuada nisl sed cursus. Etiam varius metus quam, non posuere diam sodales at. Aenean tincidunt turpis orci. Sed sed lectus ac urna lacinia efficitur nec nec dolor. Duis ex nulla, tempor id gravida iaculis, lobortis vel risus. Sed ac condimentum arcu, et tristique urna. Sed interdum ligula ut sem varius pretium. Vivamus in gravida nisl, id lobortis massa. Duis lacinia augue id ante vestibulum cursus.\n' +
+		'\n' +
+		'Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce malesuada aliquam turpis, eget dignissim justo facilisis in. Donec lorem tortor, tempor id arcu ut, aliquam vulputate erat. Proin pharetra bibendum erat, at interdum eros semper ac. Curabitur rhoncus elit orci, quis volutpat eros hendrerit nec. Etiam ultricies neque in tellus vehicula, vitae porttitor mi commodo. Proin posuere purus vitae suscipit efficitur. Fusce lorem lorem, dictum eget commodo vel, malesuada vitae augue. Pellentesque finibus, nulla at euismod maximus, odio dolor cursus mi, vel pharetra sapien mi quis orci. Praesent non enim odio. Mauris ac ex id lacus consequat cursus.'
+
 	const activeTasks = [
-		{ id: 1, title: 'Захватить мир', description: 'Описание задачи 1', date: '28 января', active: true},
-		{ id: 2, title: 'Помыть посуду', description: 'Описание задачи 2', date: '29 января', active: true},
-		{ id: 3, title: 'Убраться в комнате', description: 'Описание задачи 3', date: '30 января', active: true},
-		{ id: 4, title: 'Купить самолет', description: 'Описание задачи 4', date: '28 января', active: true},
-		{ id: 5, title: 'Угнать машину', description: 'Описание задачи 5', date: '29 января', active: true},
+		{ id: 1, title: 'Захватить мир', description: Lorem + Lorem + Lorem, date: '28 января', active: true},
+		{ id: 2, title: 'Помыть посуду', description: Lorem, date: '29 января', active: true},
+		{ id: 3, title: 'Убраться в комнате', description: Lorem, date: '', active: true},
+		{ id: 4, title: 'Купить самолет', description: Lorem, date: '28 января', active: true},
+		{ id: 5, title: 'Угнать машину', description: Lorem, date: '', active: true},
 	]
 
 	const completedTasks = [
 		{ id: 1, title: 'Купить танк', description: 'Описание задачи', date: '20 апреля', active: false },
 		{ id: 2, title: 'Починить книгу', description: 'Описание задачи', date: '20 апреля', active: false },
 		{ id: 3, title: 'Организовать бой носорогов', description: 'Описание задачи', date: '20 апреля', active: false },
-		{ id: 4, title: 'Заказать такси', description: 'Описание задачи', date: '20 апреля', active: false },
+		{ id: 4, title: 'Заказать такси', description: 'Описание задачи', date: '', active: false },
 		{ id: 5, title: 'Почитать старших', description: 'Описание задачи', date: '20 апреля', active: false },
 		{ id: 6, title: 'Устроиться на работу', description: 'Описание задачи', date: '20 апреля', active: false },
 		{ id: 7, title: 'Взломать сейф', description: 'Описание задачи', date: '20 апреля', active: false },
 		{ id: 8, title: 'Покоримить крокодила', description: 'Описание задачи', date: '20 апреля', active: false },
-		{ id: 9, title: 'Ловить мух', description: 'Описание задачи', date: '20 апреля', active: false },
+		{ id: 9, title: 'Ловить мух', description: 'Описание задачи', date: '', active: false },
 		{ id: 10, title: 'Покрасить зебру', description: 'Описание задачи', date: '20 апреля', active: false },
 		{ id: 11, title: 'Сходить в магазин', description: 'Описание задачи', date: '20 апреля', active: false },
 	]
--- src/components/Todo/RightContent/TaskItem/TaskItem.jsx
+++ src/components/Todo/RightContent/TaskItem/TaskItem.jsx
@@ -1,34 +1,48 @@
 import React from 'react'
 import style from './style.css'
+import calendar from '../../../../assets/svg/calendar-symbol.svg'
 
 const TaskItem = ({ id, title, description, date, active, selected, changeSelected}) => {
 	const handleCheckBox = () => {}
 
 	return (
-		<div
-			 onClick={id === selected ? () => changeSelected(0) : () => changeSelected(id)}
-			 className={selected === id? 'item selected' : 'item'}
-		>
-			<div className='checkbox'>
-				<input type='checkbox' id={id + '__label'} defaultChecked={!active} />
-				<label htmlFor={id + '__label'} onClick={() => handleCheckBox()}>
-					<svg width='11' height='8' viewBox='0 0 11 8' fill='none'>
-						<path
-							d='M9.29999 1.20001L3.79999 6.70001L1.29999 4.20001'
-							stroke='white'
-							stroke-width='1.5'
-							stroke-linecap='round'
-							stroke-linejoin='round'
-						/>
-					</svg>
-				</label>
-			</div>
-			<div style={{ marginLeft: 15 }}>
-				<div style={{ fontFamily: 'Roboto', fontSize: 18, marginBottom: 5 }}>
+		<div onClick={id === selected ? () => changeSelected(0) : () => changeSelected(id)}>
+			<div className={selected === id? 'item selected' : 'item'}>
+				<div className='checkbox'>
+					<input type='checkbox' id={id + '__label'} defaultChecked={!active} />
+					<label htmlFor={id + '__label'} onClick={() => handleCheckBox()}>
+						<svg width='11' height='8' viewBox='0 0 11 8' fill='none'>
+							<path
+								d='M9.29999 1.20001L3.79999 6.70001L1.29999 4.20001'
+								stroke='white'
+								stroke-width='1.5'
+								stroke-linecap='round'
+								stroke-linejoin='round'
+							/>
+						</svg>
+					</label>
+				</div>
+				<div className='task-name'>
 					{active? title : <s>{title}</s>}
 				</div>
-				<div style={{ fontFamily: 'Roboto', fontSize: 12, color: '#363636' }}>
-					До {date}
+			</div>
+			<div className='task-date' style={selected === id? {display: 'none'} : {}}>
+				{date ? 'до ' + date : ''}
+			</div>
+			<div className='task-additionally' style={selected === id? {marginTop: 10} : {display: 'none'}}>
+				<div className='task-description' style={{padding: 20, backgroundColor: '#F1F1F1'}}>
+					{description}
+				</div>
+				<div className='task-additionally-bottom'>
+					<div style={{color: '#FF5F5F'}}>
+						Удалить задачу
+					</div>
+					<div>
+						<div style={{display: 'flex', alignItems: 'center'}}>
+							<img src={calendar} alt="" style={{marginRight: 10}}/>
+							<span>{date ? 'до ' + date : 'Назначить дату'}</span>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
