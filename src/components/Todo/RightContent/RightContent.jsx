3decd98957a5b24a76277a91b4904d36b5c4f062
Haleckiy <haleckiydenisirl@gmail.com>
2022-05-08 17:28:42 +0700

    Изменен вид выполненных задач. Изменена логика отображения выполненных задач.

--- src/components/Todo/RightContent/RightContent.jsx
+++ src/components/Todo/RightContent/RightContent.jsx
@@ -8,38 +8,27 @@ import UiAccordion from '../../ui-kit/accordion/UiAccordion/UiAccordion'
 import ModalWindow from "./ModalWindow/ModalWindow";
 
 const RightContent = () => {
-	const indexes = [1, 2, 3, 4]
 
-	const completedTaskItem = title => {
-		return (
-			<div
-				style={{
-					display: 'flex',
-					alignItems: 'center',
-					marginBottom: 20,
-					marginLeft: 5,
-				}}
-			>
-				<img src={svgIcon.checkMark} alt='' width={20} />
-				<div style={{ marginLeft: 20 }}>
-					<s>{title}</s>
-				</div>
-			</div>
-		)
-	}
+	const activeTasks = [
+		{ id: 1, title: 'Захватить мир', description: 'Описание задачи 1', date: '28 января', active: true},
+		{ id: 2, title: 'Помыть посуду', description: 'Описание задачи 2', date: '29 января', active: true},
+		{ id: 3, title: 'Убраться в комнате', description: 'Описание задачи 3', date: '30 января', active: true},
+		{ id: 4, title: 'Купить самолет', description: 'Описание задачи 4', date: '28 января', active: true},
+		{ id: 5, title: 'Угнать машину', description: 'Описание задачи 5', date: '29 января', active: true},
+	]
 
-	const arr = [
-		{ id: 1, text: 'Задача 1' },
-		{ id: 2, text: 'Задача 2' },
-		{ id: 3, text: 'Задача 3' },
-		{ id: 4, text: 'Задача 1' },
-		{ id: 5, text: 'Задача 2' },
-		{ id: 6, text: 'Задача 3' },
-		{ id: 7, text: 'Задача 1' },
-		{ id: 8, text: 'Задача 2' },
-		{ id: 9, text: 'Задача 3' },
-		{ id: 10, text: 'Задача 3' },
-		{ id: 11, text: 'Задача 3' },
+	const completedTasks = [
+		{ id: 1, title: 'Купить танк', description: 'Описание задачи', date: '20 апреля', active: false },
+		{ id: 2, title: 'Починить книгу', description: 'Описание задачи', date: '20 апреля', active: false },
+		{ id: 3, title: 'Организовать бой носорогов', description: 'Описание задачи', date: '20 апреля', active: false },
+		{ id: 4, title: 'Заказать такси', description: 'Описание задачи', date: '20 апреля', active: false },
+		{ id: 5, title: 'Почитать старших', description: 'Описание задачи', date: '20 апреля', active: false },
+		{ id: 6, title: 'Устроиться на работу', description: 'Описание задачи', date: '20 апреля', active: false },
+		{ id: 7, title: 'Взломать сейф', description: 'Описание задачи', date: '20 апреля', active: false },
+		{ id: 8, title: 'Покоримить крокодила', description: 'Описание задачи', date: '20 апреля', active: false },
+		{ id: 9, title: 'Ловить мух', description: 'Описание задачи', date: '20 апреля', active: false },
+		{ id: 10, title: 'Покрасить зебру', description: 'Описание задачи', date: '20 апреля', active: false },
+		{ id: 11, title: 'Сходить в магазин', description: 'Описание задачи', date: '20 апреля', active: false },
 	]
 
 	const [modalActive, setModalActive] = useState(false)
@@ -56,8 +45,8 @@ const RightContent = () => {
 				</div>
 			</div>
 			<div style={{ marginTop: 40 }}>
-				{indexes.map(item => (
-					<TaskItem idI={item} />
+				{activeTasks.map(item => (
+					<TaskItem id={item.id} title={item.title} description={item.description} date={item.date} active={item.active}/>
 				))}
 			</div>
 			<div style={{ display: 'flex', marginLeft: 7, marginTop: 40 }}>
@@ -68,9 +57,8 @@ const RightContent = () => {
 				<hr color='#C4C4C4' size='1' style={{ marginTop: 30 }} />
 			</div>
 			<UiAccordion
-				title={'65 задач выполнено'}
-				tasks={arr}
-				renderItem={completedTaskItem}
+				title={completedTasks.length + ' задач выполнено'}
+				tasks={completedTasks}
 			/>
 		</div>
 	)
--- src/components/Todo/RightContent/TaskItem/TaskItem.jsx
+++ src/components/Todo/RightContent/TaskItem/TaskItem.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import style from './style.scss'
 
-const TaskItem = ({ idI }) => {
+const TaskItem = ({ id, title, description, date, active}) => {
 	const handleCheckBox = () => {}
 
 	return (
 		<div style={{ display: 'flex', marginBottom: 25 }}>
 			<div className='checkbox'>
-				<input type='checkbox' id={idI + '__label'} defaultChecked={false} />
-				<label htmlFor={idI + '__label'} onClick={() => handleCheckBox()}>
+				<input type='checkbox' id={id + '__label'} defaultChecked={!active} />
+				<label htmlFor={id + '__label'} onClick={() => handleCheckBox()}>
 					<svg width='11' height='8' viewBox='0 0 11 8' fill='none'>
 						<path
 							d='M9.29999 1.20001L3.79999 6.70001L1.29999 4.20001'
@@ -22,10 +22,10 @@ const TaskItem = ({ idI }) => {
 			</div>
 			<div style={{ marginLeft: 15 }}>
 				<div style={{ fontFamily: 'Roboto', fontSize: 18, marginBottom: 5 }}>
-					Задача {idI}
+					{active? title : <s>{title}</s>}
 				</div>
 				<div style={{ fontFamily: 'Roboto', fontSize: 12, color: '#363636' }}>
-					До 28 февраля
+					До {date}
 				</div>
 			</div>
 		</div>
