7ce31f069d176ef074f7febac37fdc1e8a173ca0
lans q <aptem40@gmail.com>
2022-05-11 08:58:35 +0700

    Сделано добавление и удаление категорий

--- src/components/Todo/LeftContent/LeftContent.jsx
+++ src/components/Todo/LeftContent/LeftContent.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
 import style from './style.css'
 import { svgIcon } from '../../../assets/svg'
+import { editableInputTypes } from '@testing-library/user-event/dist/utils'
 
 const LeftContent = () => {
-	const mas = [
+	const [mas, changeMas] = useState([
 		{
 			id: 1,
 			text: 'Категория 1',
@@ -12,9 +13,9 @@ const LeftContent = () => {
 		{ id: 3, text: 'Категория 3' },
 		{ id: 4, text: 'Категория 4' },
 		{ id: 5, text: 'Категория 5' },
-	]
+	])
 	let [highlighted, setHighlighted] = useState(1)
-
+	
 	return (
 		<div className='left-content-container'>
 			<div className='left-content-topContainer'>
@@ -32,6 +33,20 @@ const LeftContent = () => {
 									<img src={svgIcon.dot} width={12} />
 								</div>
 								{item.text}
+								<a href="#" class="close" 
+									onClick= {() => 
+										{
+											if (window.confirm('Вы точно хотите удалить эту категорию?')) {
+												for (let i = 0; i < mas.length; i++) {
+													if (mas[i].id === item.id) {
+														mas.splice(i--, 1);
+													}
+												}
+												changeMas([...mas])
+											};
+										}
+									}
+								></a>
 							</div>
 						) : (
 							<div
@@ -47,7 +62,7 @@ const LeftContent = () => {
 					)}
 				</div>
 			</div>
-			<div className='left-content-botContainer'>
+			<div className='left-content-botContainer' onClick={() => changeMas([...mas, { id: mas[mas.length - 1].id + 1, text:"Категория " + (mas[mas.length - 1].id + 1)}])}>
 				<div className='left-content__addIcon'>
 					<img src={svgIcon.plus} width={20} />
 				</div>
