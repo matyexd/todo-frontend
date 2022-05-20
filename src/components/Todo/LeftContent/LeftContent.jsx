c3e68d293eaef4b4fa6cfad9e2f219bffdf0afbe
Haleckiy <haleckiydenisirl@gmail.com>
2022-05-20 21:27:48 +0700

    Исправлено позиционирование крестика при выбранной категории, изменены стили редактирования названия категории

--- src/components/Todo/LeftContent/LeftContent.jsx
+++ src/components/Todo/LeftContent/LeftContent.jsx
@@ -43,30 +43,35 @@ const LeftContent = () => {
 				<div className='left-content__middle'>
 					{mas.map(item =>
 						item.id === highlighted ? (
-							<div className='left-content__item' style={{ backgroundColor: '#DBDBDB' }}>
-								<div className='left-content__itemImg'>
-									<img src={svgIcon.dot} width={12} />
-								</div>
-								{item.id === editable ? (
-								<form id="form" onSubmit={() => removeFunc(item.id)}>
-									<input type="text" value={categoryName} onChange={(e) => setCategoryName(e.target.value)}/>
-								</form>
-								) : (
-								<div className="item-text" onDoubleClick={() => { setCategoryName(item.text); setEditable(item.id) } }>{item.text}</div>)}
-								<a href="#" class="close" 
-									onClick= {() => 
-										{
-											if (window.confirm('Вы точно хотите удалить эту категорию?')) {
-												for (let i = 0; i < mas.length; i++) {
-													if (mas[i].id === item.id) {
-														mas.splice(i--, 1);
-													}
-												}
-												changeMas([...mas])
-											};
+							<div className='left-content__item__highlighted' style={{ backgroundColor: '#DBDBDB' }}>
+								<div>
+									<div className='left-content__flex-box'>
+										<div className='left-content__itemImg'>
+											<img src={svgIcon.dot} width={12} />
+										</div>
+										{item.id === editable ? (
+											<form className="item-text" id="form" onSubmit={() => removeFunc(item.id)}>
+												<input className='item-text-input' type="text" value={categoryName} onChange={(e) => setCategoryName(e.target.value)}/>
+											</form>
+										) : (
+											<div className="item-text" onDoubleClick={() => { setCategoryName(item.text); setEditable(item.id) } }>{item.text}</div>)
 										}
-									}
-								></a>
+									</div>
+								</div>
+								<div
+									className="close"
+									onClick= {() => {
+										if (window.confirm('Вы точно хотите удалить эту категорию?')) {
+											for (let i = 0; i < mas.length; i++) {
+												if (mas[i].id === item.id) {
+													mas.splice(i--, 1);
+												}
+											}
+											changeMas([...mas])
+										}
+									}}
+								>
+								</div>
 							</div>
 						) : (
 							<div
